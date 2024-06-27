e9882028e97dd218d03b930313ed0621f902c937
Araneae10 <150362684+Araneae10@users.noreply.github.com>
2024-06-27 11:34:15 +0545

    Add files via upload

---
+++ random gussing.py
@@ -0,0 +1,29 @@
+import random
+import math
+
+lower = int(input('enter the lower bound:'))
+
+upper = int(input('enter the upper bound'))
+
+x= random.randint(lower,upper)
+
+print('you have only' ,round(math.log(upper - lower + 1,2)),'try')
+
+count = 0
+
+while True:
+    count += 1
+    guess = int(input(f'guess a number between {lower} and {upper}:'))
+    
+
+    if x == guess:
+        print(f'\ncongratualtion you did it in {count} try')
+        break
+    elif x>guess:
+        print('\nyour guess is too low')
+    elif x<guess:
+        print('\nyour guess is to high')
+
+if count >=math.log(upper-lower+1,2):
+    print('\n the number is %d'%x)
+    print('\n better luck next time')
