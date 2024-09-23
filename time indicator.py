c1f7d8b61923158ae09b524cea9077cd5a7fa66c
Araneae10 <150362684+Araneae10@users.noreply.github.com>
2024-09-23 10:11:09 +0545

    Update time indicator.py

--- time indicator.py
+++ time indicator.py
@@ -1,28 +1,30 @@
-question=('what is the capital of nepal?','How many province are there in nepal?')
-answer=('a','c')
-print('the amount you can win is:1000 to 10000000')
+# import datetime
 
-print(question[0])
-print("the option are")
-print('a)kathmandu')
-print('b)bhaktapur')
-print('c)itahari')
-print('d)lalitpur')
-ans=(input('enter your option:  '))
-if ans.lower() == answer[0]:
-    print('you win RS1000')
-else:
-    print('you lose')
+# def get_greeting():
+#     current_time= datetime.datetime.now()
+#     hour =current_time.hour
 
+#     if hour < 12:
+#         return "Good Morning!"
+#     elif hour < 15:
+#         return "Good Afternoon!"
+#     else:
+#         return "Good Evening!"
 
-print(question[1])
-print("the option are")
-print('a)10')
-print('b)5')
-print('c)7')
-print('d)3')
-ans=(input('enter your option:  '))
-if ans.lower() == answer[1]:
-    print('you win RS10000')
-else:
-    print('you lose')
+# # print(f"{get_greeting()} Current time is {current_time.strftime('%I:%M %p')}")
+import time
+import tkinter as tk
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+    root.after(1000, update_time)
+
+root = tk.Tk()
+root.title("Digital Clock")
+
+clock_label = tk.Label(root, font=('calibri', 40, 'bold'), background='purple', foreground='white')
+clock_label.pack(fill='both', expand=1)
+
+update_time()
+root.mainloop()
